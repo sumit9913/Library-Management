051e9d899870d634ff90304f214b75a9668728d1
SUMIT MAURYA <sumit.m992@gmail.com>
2021-05-09 18:40:06 +0530

    spaace

---
+++ Lib1/adminpublishermanagement.aspx.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lib1
+{
+    public partial class adminpublishermanagement : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+        }
+        //Add
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                Response.Write("<script>alert('Publisher with this ID already exists');</script>");
+            }
+            else
+            {
+                addNewPublisher();
+            }
+        }
+        //Update
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                updatePublisher();
+            }
+            else
+            {
+                Response.Write("<script>alert('Publisher with this ID does not exists');</script>");
+            }
+        }
+        //Delete
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (checkIfPublisherExists())
+            {
+                deletePublisher();
+            }
+            else
+            {
+                Response.Write("<script>alert('Publisher with this ID does not exists');</script>");
+            }
+        }
+        //Search
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getPublisherByID();
+        }
+
+        //user defined class
+        bool checkIfPublisherExists()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if(con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("SELECT * from publisher_master_tbl where publisher_id ='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+        }
+
+        void addNewPublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                SqlCommand cmd = new SqlCommand("INSERT INTO publisher_master_tbl" +
+                    "(publisher_id,publisher_name) values" +
+                    "(@publisher_id,@publisher_name)", con);
+
+                cmd.Parameters.AddWithValue("@publisher_id", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@publisher_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Publisher added Successfully.');</script>");
+                clearform();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        void updatePublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("UPDATE publisher_master_tbl SET publisher_name=@publisher_name WHERE publisher_id='" + TextBox1.Text.Trim() + "'", con);
+
+                cmd.Parameters.AddWithValue("@publisher_name", TextBox2.Text.Trim());
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Publisher Updated Successfully');</script>");
+                clearform();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void deletePublisher()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("DELETE from publisher_master_tbl WHERE publisher_id='" + TextBox1.Text.Trim() + "'", con);
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Publisher Deleted Successfully');</script>");
+                clearform();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void getPublisherByID()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from publisher_master_tbl where publisher_id='" + TextBox1.Text.Trim() + "';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count >= 1)
+                {
+                    TextBox2.Text = dt.Rows[0][1].ToString();
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid Publisher ID');</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+
+            }
+        }
+        void clearform()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+        }
+    }
+}
---
+++ Lib1/homepage.aspx
@@ -0,0 +1,95 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="Lib1.Homepage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <section>
+        <img src="img/home-bg.jpg" class="img-fluid"/>
+    </section>
+
+    <section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <center>
+                        <h2>Our Features</h2>
+                        <p><b>Our 3 Primary Features -</b></p>
+                    </center>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/digital-inventory.png" />
+                        <h4>Digital Book LIbrary</h4>
+                        <p class="text-justify">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/search-online.png" />
+                        <h4>Search Books</h4>
+                        <p class="text-justify">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/defaulters-list.png" />
+                        <h4>Defaulter List</h4>
+                        <p class="text-justify">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section>
+        <img src="img/in-homepage-banner.jpg" class="img-fluid"/>
+    </section>
+    
+    <section>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <center>
+                        <h2>Our Process</h2>
+                        <p><b>We have simple 3 step process -</b></p>
+                    </center>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/sign-up.png" />
+                        <h4>Sign up</h4>
+                        <p class="text-justify">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/search-online.png" />
+                        <h4>Search Books</h4>
+                        <p class="text-justify">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="150px" src="img/library.png" />
+                        <h4>Visit us</h4>
+                        <p class="text-wrap">Just a few line description for this section,Digital library it is. </p>
+                    </center>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+</asp:Content>
